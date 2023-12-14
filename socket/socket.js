4b89101aa6b0a42990e5fcd9a01807a06d185bfb
ameer017 <rajiabdullahi907@gmail.com>
2023-12-14 12:37:36 +0100

    last__

--- socket/socket.js
+++ socket/socket.js
@@ -8,9 +8,14 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: "http://localhost:3000",
-		methods: ["GET", "POST"],
-	},
+		origin: [
+		  "http://localhost:3000",
+		  "https://threads-frontend-ten.vercel.app", 
+		  "*"
+	  ],
+		credentials: true,
+		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', 
+	}
 });
 
 export const getRecipientSocketId = (recipientId) => {
