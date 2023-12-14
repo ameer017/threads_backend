4865a2b9f4322bcebdf2dea44193757caf5ba386
ameer017 <rajiabdullahi907@gmail.com>
2023-12-14 12:45:03 +0100

    socket.io

--- socket/socket.js
+++ socket/socket.js
@@ -8,15 +8,12 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: [
-		  "http://localhost:3000",
-		  "https://threads-frontend-ten.vercel.app", 
-		  "*"
-	  ],
-		credentials: true,
-		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', 
+	  origin: ["http://localhost:3000", "https://threads-frontend-ten.vercel.app", "*"],
+	  credentials: true,
+	  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE'
 	}
-});
+  });
+  
 
 export const getRecipientSocketId = (recipientId) => {
 	return userSocketMap[recipientId];
